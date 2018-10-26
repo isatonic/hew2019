6e32d6a79770b33375aa75e7d9c4b70185c61340
tksn-jp <tk5nj9@gmail.com>
2018-10-26 11:50:08 +0900

    add 2 method (regist, udpate) to Users

--- model/physical/Users.php
+++ model/physical/Users.php
@@ -2,4 +2,40 @@
 
 class Users extends ModelBase {
 
+    /**
+     * regist: ユーザ登録
+     *      (参照: ./ModelBase.php)
+     *
+     * @param $data
+     *      $data[列名] = 値
+     *
+     * @return bool
+     */
+    public function regist($data) {
+        $res = $this->insert($data);
+        return $res;
+    }
+
+    /**
+     * update: ユーザ情報の更新
+     *
+     * @param $data
+     *      $data[列名] = 値
+     * @param $who
+     *      メールアドレス
+     *
+     * @return bool
+     */
+    public function update($data, $who) {
+        $keyval = array();
+        foreach ($data as $key => $val) {
+            $keyval[] = "${key}=${val}";
+        }
+        $sql = sprintf("UPDATE %s SET", $this->table_name);
+        $sql = sprintf("$sql %s WHERE email LIKE %s", implode(", ", $keyval), $who);
+        $stmt = $this->db->prepare($sql);
+        $res = $stmt->execute();
+        return $res;
+    }
+
 }
