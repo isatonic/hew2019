df9e4de819fbfbd72231ab17c91c929107f2969a
tksn-jp <tk5nj9@gmail.com>
2018-11-01 13:36:25 +0900

    Add method to regist image
    Productsクラスに画像登録用の関数registを追加

--- model/physical/Products.php
+++ model/physical/Products.php
@@ -2,4 +2,22 @@
 
 class Products extends ModelBase {
 
+    /**
+     * regist: 画像登録
+     *
+     * @param $data[]
+     *      id: String              -> 作品ID
+     *      fileName: String        -> ファイル名
+     *      title: String           -> タイトル
+     *      author: String          -> 投稿者メールアドレス
+     *      price: int              -> 価格
+     *      authorComment: String   -> 投稿者コメント
+     *
+     * @return bool
+     */
+    public function regist($data) {
+        $res = $this->insert($data);
+        return $res;
+    }
+
 }
