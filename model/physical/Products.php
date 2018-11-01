508516c1439b40981163de996fb70c8256d022c9
tksn-jp <tk5nj9@gmail.com>
2018-11-01 14:33:15 +0900

    Add function fetchAllImage
    Productsクラスに関数fetchAllImageを追加.
    (全画像データの取得)

--- model/physical/Products.php
+++ model/physical/Products.php
@@ -20,4 +20,22 @@ class Products extends ModelBase {
         return $res;
     }
 
+    /**
+     * fetchAllImage: 全画像データの取得
+     *
+     * @return mixed[]: 連想配列
+     *      id: String              -> 作品ID
+     *      fileName: String        -> ファイル名
+     *      title: String           -> タイトル
+     *      author: String          -> 投稿者メールアドレス
+     *      postDate: String        -> 投稿日時
+     *      price: int              -> 価格
+     *      authorComment: String   -> 投稿者コメント
+     */
+    public function fetchAllImage() {
+        $sql = "SELECT id, fileName, title, author, postDate, price, authorComment";
+        $rows = $this->query($sql);
+        return $rows;
+    }
+
 }
