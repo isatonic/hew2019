5d351d2d55782195aa76ae625194ec587d90ab05
tksn-jp <tk5nj9@gmail.com>
2018-11-01 20:56:26 +0900

    Add method in Cart
    Cartクラスにadd(), remove()を追加

--- model/physical/Cart.php
+++ model/physical/Cart.php
@@ -2,4 +2,41 @@
 
 class Cart extends ModelBase {
 
+    /**
+     * add: カートに商品を追加
+     *
+     * @param $user
+     * @param $product
+     *
+     * @return bool
+     */
+    public function add($user, $product) {
+        $data = array(
+            "product" => $product,
+            "user" => $user
+        );
+        $res = $this->insert($data);
+
+        return $res;
+    }
+
+    /**
+     * remove: カートから商品を削除
+     *
+     * @param $user
+     * @param $product
+     *
+     * @return bool
+     */
+    public function remove($user, $product) {
+        $where = "user LIKE :user and product LIKE :product";
+        $params = array(
+            "user" => $user,
+            "product" => $product
+        );
+        $res = $this->delete($where, $params);
+
+        return $res;
+    }
+
 }
