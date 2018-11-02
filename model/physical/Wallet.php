552110049e6a40fcad040d150e3bfe0fd9625971
tksn-jp <tk5nj9@gmail.com>
2018-11-02 11:26:52 +0900

    Add 3 methods in class Wallet
    WalletクラスにcheckWallet(), charge(), userPoint()を追加

--- model/physical/Wallet.php
+++ model/physical/Wallet.php
@@ -2,4 +2,66 @@
 
 class Wallet extends ModelBase {
 
+    /**
+     * checkWallet: ウォレット(所持Tポイント)を取得
+     *
+     * @param $user
+     *
+     * @return int
+     */
+    public function checkWallet($user) {
+        $sql = "SELECT point FROM Wallet WHERE user LIKE :user";
+        $params = array(
+            "user" => $user
+        );
+        $rows = $this->query($sql, $params);
+        $point = $rows[0]["point"];
+
+        return $point;
+    }
+
+    /**
+     * charge: Tポイントをウォレットに追加
+     *
+     * @param $user
+     * @param $point
+     *      追加分ポイント
+     *
+     * @return bool
+     */
+    public function charge($user, $point) {
+        $newPoint = $this->checkWallet($user) + $point;
+        $data = array(
+            "point" => $newPoint
+        );
+        $where = "WHERE user LIKE $user";
+        $res = $this->update($data, $where);
+
+        return $res;
+    }
+
+    /**
+     * usePoint: Tポイントを消費
+     *
+     * @param $user
+     * @param $point
+     *      消費分ポイント
+     *
+     * @return bool
+     */
+    public function usePoint($user, $point) {
+        $newPoint = $this->checkWallet($user) - $point;
+        if ($newPoint < 0) {
+            $res = false;
+        } else {
+            $data = array(
+                "point" => $newPoint
+            );
+            $where = "WHERE user LIKE $user";
+            $res = $this->update($data, $where);
+        }
+
+        return $res;
+
+    }
 }
