174cccf0053924dce6542b9a35e3cffbfd0dfbfe
Yuchopa <yudai_0539@yahoo.co.jp>
2018-11-02 21:06:03 +0900

    <SN1-2>スクロール対応/順に表示可能に

--- html/js/popup.js
+++ html/js/popup.js
@@ -1,39 +1,47 @@
 //Messeage List
 $(function () {
-  $('#openModal').click(function(){
-      $('#modalArea').fadeIn();
-  });
-  $('#closeModal , #modalBg').click(function(){
+  $('#openModal').click
+  (function(){
+      $('#modalArea').fadeIn(); 
+  });	
+  $('#closeModal , #modalBg').click
+  (function(){
     $('#modalArea').fadeOut();
   });
 });
 
 //Friend List
 $(function () {
-  $('#openModal_Friend_List').click(function(){
+  $('#openModal_Friend_List').click
+  (function(){
       $('#modalArea_Friend_List').fadeIn();
   });
-  $('#closeModal_Friend_List , #modalBg_Friend_List').click(function(){
+  $('#closeModal_Friend_List , #modalBg_Friend_List').click
+  (function(){
     $('#modalArea_Friend_List').fadeOut();
   });
 });
 
 //Friend Select
 $(function () {
-  $('#openModal_Friend_Select').click(function(){
+  $('#openModal_Friend_Select').click
+  (function(){
       $('#modalArea_Friend_Select').fadeIn();
   });
-  $('#closeModal_Friend_Select , #modalBg_Friend_Select').click(function(){
+  $('#closeModal_Friend_Select , #modalBg_Friend_Select').click
+  (function(){
     $('#modalArea_Friend_Select').fadeOut();
   });
 });
 
 //Friend Search
 $(function () {
-  $('#openModal_Friend_Search').click(function(){
+  $('#openModal_Friend_Search').click
+  (function(){
       $('#modalArea_Friend_Search').fadeIn();
   });
-  $('#closeModal_Friend_Search , #modalBg_Friend_Search').click(function(){
+  $('#closeModal_Friend_Search , #modalBg_Friend_Search').click
+  (function(){
     $('#modalArea_Friend_Search').fadeOut();
   });
 });
