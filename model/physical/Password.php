85934ca58dd923d1335d16a5748aca9b34bf28b6
tksn-jp <tk5nj9@gmail.com>
2018-11-02 12:49:21 +0900

    Add 3 methods in class Password
    `Password`クラスに`regist()`, `check()`, `change()`を追加

--- model/physical/Password.php
+++ model/physical/Password.php
@@ -2,4 +2,59 @@
 
 class Password extends ModelBase {
 
+    /**
+     * regist: パスワード登録
+     *      ユーザ登録時にUsers.registと同トランザクション内で実行
+     *
+     * @param $user
+     * @param $pass
+     *
+     * @return bool
+     */
+    public function regist($user, $pass) {
+        $data = array(
+            "user" => $user,
+            "pass" => password_hash($pass, PASSWORD_DEFAULT)
+        );
+        $res = $this->insert($data);
+
+        return $res;
+    }
+
+    /**
+     * check: パスワード照合
+     *
+     * @param $user
+     * @param $pass
+     *
+     * @return bool
+     */
+    public function check($user, $pass) {
+        $sql = "SELECT pass FROM Password WHERE user LIKE :user";
+        $params["user"] = $user;
+        $res = $this->query($sql, $params);
+        $hash = $res[0]["pass"];
+
+        return password_verify($pass, $hash);
+    }
+
+    /**
+     * change: パスワード変更
+     *
+     * @param $user
+     * @param $pass
+     *      新しいパスワード
+     *
+     * @return bool
+     */
+    public function change($user, $pass) {
+        $data = array(
+            "pass" => password_hash($pass, PASSWORD_DEFAULT)
+        );
+        $where = "user LIKE $user";
+        $res = $this->update($data, $where);
+
+        return $res;
+    }
+
 }
