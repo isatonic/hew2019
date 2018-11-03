b565afc6ed51b60004c60cd6b08eb003071c6bea
tksn-jp <tk5nj9@gmail.com>
2018-11-03 14:38:54 +0900

    Add method in class Purchase
    Purchaseクラスに購入履歴を保存するbuy()を追加

--- model/physical/Purchase.php
+++ model/physical/Purchase.php
@@ -2,4 +2,21 @@
 
 class Purchase extends ModelBase {
 
+    /**
+     * buy: 購入履歴の追加
+     *
+     * @param $user
+     * @param $product
+     *
+     * @return bool
+     */
+    public function buy($user, $product) {
+        $data = array(
+            "buyer" => $user,
+            "product" => $product
+        );
+
+        return $this->insert($data);
+    }
+
 }
