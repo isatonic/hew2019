9551e75071708948b2850d4abe1605801383fd9f
tksn-jp <tk5nj9@gmail.com>
2018-11-08 14:08:34 +0900

    Add methods in class PassReset
    PassResetクラスに以下のメソッドを追加
    - リセット要求を登録する add()
    - 最新のリセット要求を取得する get()

--- model/physical/PassReset.php
+++ model/physical/PassReset.php
@@ -9,4 +9,45 @@ namespace model\physical;
  */
 class PassReset extends ModelBase {
 
+    /**
+     * パスワードリセット要求を設定
+     *
+     * @param string $who       ユーザのメールアドレス
+     * @param string $newPass   新しいパスワード
+     *
+     * @return bool
+     */
+    public function add(string $who, string $newPass) {
+        $data = array(
+            "user" => $who,
+            "newpass" => $newPass,
+            "code" => $this->makeRandStr(4),
+            "resetLimit" => $this->setLimit(30)
+        );
+
+        return $this->insert($data);
+    }
+
+    /**
+     * 最新のリセット要求を取得
+     *
+     * @param string $who ユーザのメールアドレス
+     *
+     * @return string[]
+     */
+    public function get(string $who) {
+        $sql = "SELECT newpass, code, resetLimit FROM PassReset WHERE user LIKE :user ORDER BY resetLimit DESC";
+        $params = array(
+            "user" => $who
+        );
+        $rows = $this->query($sql, $params);
+        $ret = array(
+            "newPass" => $rows[0]["newpass"],
+            "code" => $rows[0]["code"],
+            "limit" => $rows[0]["resetLimit"]
+        );
+
+        return $ret;
+    }
+
 }
