a1321d21c368acd1ab83a91803f4a4d6c22e412e
tksn-jp <tk5nj9@gmail.com>
2018-11-11 14:43:52 +0900

    Add methods in class ContactTags & Tags
    - ContactTagsクラスにメソッドを追加
        - お問い合わせ種別を全て取得する getAll()
        - 新しい種別を作成する add()
        - 特定の種別名を取得する getName()
    - Tagsクラスにメソッドを追加
        - 商品タグを全て取得する getAll()
        - 新しいタグを作成する add()
        - 特定のタグ名を取得する getName()

--- model/physical/ContactTags.php
+++ model/physical/ContactTags.php
@@ -9,4 +9,72 @@ namespace model\physical;
  */
 class ContactTags extends ModelBase {
 
+    /**
+     * お問い合わせ種別を全て取得
+     *
+     * @return array {
+     *      @type array {
+     *          @type string "id"
+     *          @type string "name
+     *      }
+     * }
+     */
+    public function getAll() {
+        $sql = "SELECT id, name FROM ContactTags ORDER BY id ASC";
+        return $this->query($sql);
+    }
+
+    /**
+     * お問い合わせ種別を新規追加
+     *
+     *
+     * @param string $name 種別名
+     *
+     * @return bool
+     */
+    public function add(string $name) {
+        $sql = "SELECT id, name FROM ContactTags ORDER BY id DESC";
+        $rows = $this->query($sql);
+
+        if ($rows == null) {
+            $id = "C0001";
+        } else {
+            $newIdNum = (int)substr($rows[0]["id"], -4) + 1;
+            $id = "C" . (string)$newIdNum;
+        }
+
+        if (strlen($id) <= 5) {
+            $data = array("id" => $id,
+                "name" => $name
+            );
+            return $this->insert($data);
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * 特定の種別の種別名を取得
+     *
+     * 引数に指定したIDの種別が存在しない場合`false`を返す
+     *
+     * @param string $id 種別ID
+     *
+     * @return bool|string
+     */
+    public function getName(string $id) {
+        $sql = "SELECT name FROM ContactTags WHERE id LIKE :id";
+        $params = array(
+            "id" => $id
+        );
+        $rows = $this->query($sql, $params);
+
+        if ($rows == null) {
+            return false;
+        } else {
+            return $rows[0]["name"];
+        }
+    }
+
 }
--- model/physical/Tags.php
+++ model/physical/Tags.php
@@ -8,4 +8,73 @@ namespace model\physical;
  * @package model\physical
  */
 class Tags extends ModelBase {
+
+    /**
+     * 商品用タグを全て取得
+     *
+     * @return array {
+     *      @type array {
+     *          @type string "id"
+     *          @type string "name"
+     *      }
+     * }
+     */
+    public function getAll() {
+        $sql = "SELECT id, name FROM Tags ORDER BY id ASC";
+        return $this->query($sql);
+    }
+
+    /**
+     * 商品用タグを新規追加
+     *
+     *
+     * @param string $name タグ名
+     *
+     * @return bool
+     */
+    public function add(string $name) {
+        $sql = "SELECT id, name FROM Tags ORDER BY id DESC";
+        $rows = $this->query($sql);
+
+        if ($rows == null) {
+            $id = "T00001";
+        } else {
+            $newIdNum = (int)substr($rows[0]["id"], -5) + 1;
+            $id = "T" . (string)$newIdNum;
+        }
+
+        if (strlen($id) <= 5) {
+            $data = array(
+                "id" => $id,
+                "name" => $name
+            );
+            return $this->insert($data);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 特定のタグのタグ名を取得
+     *
+     * 引数に指定したIDのタグが存在しない場合`false`を返す
+     *
+     * @param string $id タグID
+     *
+     * @return bool|string
+     */
+    public function getName(string $id) {
+        $sql = "SELECT name FROM Tags WHERE id LIKE :id";
+        $params = array(
+            "id" => $id
+        );
+        $rows = $this->query($sql, $params);
+
+        if ($rows == null) {
+            return false;
+        } else {
+            return $rows[0]["name"];
+        }
+    }
+
 }
