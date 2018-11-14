1f1d96ad6e86e7c339ccb5584659b4a75dae5352
tksn-jp <tk5nj9@gmail.com>
2018-11-14 14:01:09 +0900

    Add methods in class Limits
    Limitsクラスにメソッドを追加
    
    - getAll(): 制限の一覧取得
    - get(): 特定の制限を取得
    - add(): 制限を新規追加

--- model/physical/Limits.php
+++ model/physical/Limits.php
@@ -9,4 +9,52 @@ namespace model\physical;
  */
 class Limits extends ModelBase {
 
+    /**
+     * 制限の一覧を取得
+     *
+     * @return mixed[]
+     */
+    public function getAll() {
+        $sql = "SELECT id, duration, title FROM Limits";
+        $rows = $this->query($sql);
+        return $rows;
+    }
+
+    /**
+     * 特定の制限の情報を取得
+     *
+     * @param string $id
+     *
+     * @return bool|array
+     */
+    public function get(string $id) {
+        $sql = "SELECT duration, title FROM Limits WHERE id = :id";
+        $params = array(
+            "id" => $id
+        );
+        $rows = $this->query($sql, $params);
+        if ($rows != null) {
+            return $rows[0];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 制限を新規作成
+     *
+     * @param array $add
+     *
+     * @return bool
+     */
+    public function add(array $add) {
+        $data = array(
+            "id" => $add["id"],
+            "duration" => $add["duration"],
+            "title" => $add["title"]
+        );
+
+        return $this->insert($data);
+    }
+
 }
