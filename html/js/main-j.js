a4cb8a519f540ec27d50c82cbe9a094d92c61621
tksn-jp <tk5nj9@gmail.com>
2018-11-21 17:10:47 +0900

    refactor

--- html/js/main-j.js
+++ html/js/main-j.js
@@ -1,65 +1,74 @@
-jQuery(document).ready(function(event){
-  var isAnimating = false,
-    newLocation = '';
-    firstLoad = false;
-  
-  $('main').on('click', '[data-type="page-transition"]', function(event){
-    event.preventDefault();
-    var newPage = $(this).attr('href');
-    if( !isAnimating ) changePage(newPage, true);
-    firstLoad = true;
-  });
+jQuery(document).ready(function (event) {
+    let isAnimating = false,
+        newLocation = '',
+        firstLoad = false;
 
-  $(window).on('popstate', function() {
-  	if( firstLoad ) {
-      var newPageArray = location.pathname.split('/'),
-        newPage = newPageArray[newPageArray.length - 1];
-
-      if( !isAnimating  &&  newLocation != newPage ) changePage(newPage, false);
-    }
-    firstLoad = true;
-	});
-
-	function changePage(url, bool) {
-    isAnimating = true;
-    $('body').addClass('page-is-changing');
-    $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-    	loadNewContent(url, bool);
-      newLocation = url;
-      $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('main').on('click', '[data-type="page-transition"]', function (event) {
+        event.preventDefault();
+        let newPage = $(this).attr('href');
+        if (!isAnimating) {
+            changePage(newPage, true);
+        }
+        firstLoad = true;
     });
-    if( !transitionsSupported() ) {
-      loadNewContent(url, bool);
-      newLocation = url;
-    }
-	}
 
-	function loadNewContent(url, bool) {
-		url = ('' == url) ? 'index.html' : url;
-  	var newSection = 'cd-'+url.replace('.html', '');
-  	var section = $('<div class="cd-main-content '+newSection+'"></div>');
-  		
-  	section.load(url+' .cd-main-content > *', function(event){
-      $('main').html(section);
-      var delay = ( transitionsSupported() ) ? 1200 : 0;
-      setTimeout(function(){
-        ( section.hasClass('cd-about') ) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
-        $('body').removeClass('page-is-changing');
-        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-          isAnimating = false;
-          $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $(window).on('popstate', function () {
+        if (firstLoad) {
+            let newPageArray = location.pathname.split('/'),
+                newPage = newPageArray[newPageArray.length - 1];
+
+            if (!isAnimating && newLocation !== newPage) {
+                changePage(newPage, false);
+            }
+        } else {
+            firstLoad = true;
+        }
+    });
+
+    function changePage(url, bool) {
+        isAnimating = true;
+        $('body').addClass('page-is-changing');
+        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+            loadNewContent(url, bool);
+            newLocation = url;
+            $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
         });
+        if (!transitionsSupported()) {
+            loadNewContent(url, bool);
+            newLocation = url;
+        }
+    }
 
-        if( !transitionsSupported() ) isAnimating = false;
-      }, delay);
-      
-      if(url!=window.location && bool){
-        window.history.pushState({path: url},'',url);
-      }
-		});
-  }
+    function loadNewContent(url, bool) {
+        if (url === '') {
+            url = 'index.html';
+        }
+        let newSection = 'cd-' + url.replace('.html', '');
+        let section = $('<div class="cd-main-content ' + newSection + '"></div>');
 
-  function transitionsSupported() {
-    return $('html').hasClass('csstransitions');
-  }
+        section.load(url + ' .cd-main-content > *', function (event) {
+            $('main').html(section);
+            let delay = (transitionsSupported()) ? 1200 : 0;
+            setTimeout(function () {
+                (section.hasClass('cd-about')) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
+                $('body').removeClass('page-is-changing');
+                $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                    isAnimating = false;
+                    $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+                });
+
+                if (!transitionsSupported()) {
+                    isAnimating = false;
+                }
+            }, delay);
+
+            if (url !== window.location && bool) {
+                window.history.pushState({path: url}, '', url);
+            }
+        });
+    }
+
+    function transitionsSupported() {
+        return $('html').hasClass('csstransitions');
+    }
 });
