46fd8d7b9e4d6abfc5efffa0a59dda5cb0a09e2a
Yuchopa <yudai_0539@yahoo.co.jp>
2019-02-20 15:43:45 +0900

    BASE

---
+++ html/My Page/US20_pass_change.php
@@ -0,0 +1,106 @@
+<html>
+	<head>
+	<title>My Page</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+ 
+	<link rel="stylesheet" href="css/style-j.css">    
+	<script src="js/modernizr.js"></script> 
+	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="js/gooey.min.js"></script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/style_MyPage.css">
+	<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+	<script src="js/Mypage.js"></script>
+	</head>	
+	<body>
+    
+    <div class="cd-main-content">
+       <header class="cd-main-header animate-search">
+		<nav class="cd-main-nav-wrapper">
+        　　 <img src="img/LOGO/LOGO W.png" class="logo" alt="">
+			<a href="" class="cd-search-trigger cd-text-replace">Search</a>
+			
+			<ul class="cd-main-nav">
+				<li><a href="about.html" data-type="page-transition">ログイン&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+				<li><a href="">一覧&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+				<li><a href="">SNS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+				<li><a href="">カート</a></li>
+			</ul> 
+		</nav>
+
+		<a href="" class="cd-nav-trigger cd-text-replace">Menu<span></span></a>
+	</header>
+	
+    
+
+	<div id="search" class="cd-main-search">
+		<form action="" method="post">
+			<input type="search" placeholder="入力してください">
+
+		</form>
+
+		<div class="cd-search-suggestions">
+			<div class="news">
+				<h3>News</h3>
+				<ul>
+					<li>
+						<a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+						<h4><a class="cd-nowrap" href="">1</a></h4>
+					</li>
+
+					<li>
+						<a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+						<h4><a class="cd-nowrap" href="">2</a></h4>
+					</li>
+
+					<li>
+						<a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+						<h4><a class="cd-nowrap" href="">3</a></h4>
+					</li>
+   					<li>
+						<a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+						<h4><a class="cd-nowrap" href="">4</a></h4>
+					</li>
+                    <li>
+						<a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+						<h4><a class="cd-nowrap" href="">5</a></h4>
+					</li>
+
+				</ul>
+			</div> 
+		</div>
+
+		<a href="#0" class="close cd-text-replace">Close Form</a>
+	</div>  
+        </div>
+        <script src="js/jquery-2.1.4.js"></script>
+        <script src="js/main.js"></script>
+		<script src="js/Mypage.js"></script>
+		<div class="font">
+			<h2 class="Header">　　My Page</h2>
+				<div class="mype_main">
+					<div class="password_fin">
+						
+						<p class="pass_fin">パスワード変更完了しました。</p>
+						
+						
+					</div>
+				</div>
+<!-- フローティング操作ボタン -->
+			<a id="fab" href="US20.php">
+				<i class="fas fa-home"></i>
+			</a>
+			
+			
+			
+
+		</div>
+	</body>
+</html>
+
