ea1ce5aeaa1660e8c842889832ba0b0aececdfde
tksn-jp <tk5nj9@gmail.com>
2019-02-24 21:22:24 +0900

    Add US23 product list
    - 背景色等々はデバッグ用のまま

---
+++ html/product_list/US23_productList.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>作品一覧 | ISATONIC</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <link rel="stylesheet" href="css/style-j.css">
+  <script src="js/modernizr.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="js/gooey.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="./css/product_list.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="cd-main-content">
+  <header class="cd-main-header animate-search">
+    <nav class="cd-main-nav-wrapper">
+　　 <img src="img/LOGO/LOGO W.png" class="logo" alt="" onClick="location.href='../index/index.html'">
+
+      <!--------------------- 検索ボタン ----------------------------->
+      <a href="javascript:searchForm.submit()" class="cd-search-trigger cd-text-replace">Search</a>
+
+      <ul class="cd-main-nav">
+        <li><a href="" data-type="page-transition">ログイン&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+        <li><a href="">一覧&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+        <li><a href="../SNS/SN1.html">SNS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+        <li><a href="">カート</a></li>
+      </ul>
+    </nav>
+
+    <a href="" class="cd-nav-trigger cd-text-replace">Menu<span></span></a>
+  </header>
+
+
+  <div id="search" class="cd-main-search">
+    <form name="searchForm" action="" method="post">
+      <input type="search" placeholder="入力してください">
+
+    </form>
+
+    <div class="cd-search-suggestions">
+      <div class="news">
+        <h3>News</h3>
+        <ul>
+          <li>
+            <a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+            <h4><a class="cd-nowrap" href="">1</a></h4>
+          </li>
+
+          <li>
+            <a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+            <h4><a class="cd-nowrap" href="">2</a></h4>
+          </li>
+
+          <li>
+            <a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+            <h4><a class="cd-nowrap" href="">3</a></h4>
+          </li>
+          <li>
+            <a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+            <h4><a class="cd-nowrap" href="">4</a></h4>
+          </li>
+          <li>
+            <a class="image-wrapper" href=""><img src="img/placeholder.png" alt=""></a>
+            <h4><a class="cd-nowrap" href="">5</a></h4>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
+    <a href="#0" class="close cd-text-replace">Close Form</a>
+  </div>
+</div>
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/main.js"></script>
+
+<div class="main-content">
+
+  <!----------------------- ボタン ---------------------------->
+  <div class="button-wrap flex-container">
+      <button class="controll-button">ジャンル検索</button>
+      <button class="controll-button">新作順</button>
+      <button class="controll-button">ランキング順</button>
+  </div>
+  <!----------------------------------------------------------->
+
+  <!------------------------- 一覧 ---------------------------->
+  <div class="product-list flex-container">
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+    <a href=""><img class="product" src="./img/クレジットカードブランド.jpg" alt=""></a>
+  </div>
+  <!----------------------------------------------------------->
+
+</div>
+
+</body>
+</html>
