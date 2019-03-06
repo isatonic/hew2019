a8d6aeadd1891474ed411b3f51e8b964eee86fe1
Yuchopa <yudai_0539@yahoo.co.jp>
2019-03-06 16:58:59 +0900

    ログイン画面実装完了

--- html/login/new_member.php
+++ html/login/new_member.php
@@ -94,71 +94,71 @@
 							<h1>新規会員登録</h1>
 							<br>
 							
-						<form　method="post" action="../controller/regist.php">
+						<form method="post" action="../controller/regist.php">
 							<input type="text" placeholder="ニックネーム(2文字以上)"/>
-				<div class="centerrr">
-                <input type="text" placeholder="性" class="ee" max="10">
-                   <input type="text" placeholder="名" class="rr">
-				</div>
-
-                <input type="radio" value="男"class="aa" name="gender"><p class="cc">:男</p>
-                <input type="radio" value="女"class="bb" name="gender"><p class="dd">:女</p>
-                <br>
-                <input type="text" class="ll" placeholder="西暦"/>
-                <select name="month"style="color:#000000"class="qq" >
-                  <option value="">生月を選択してください!</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                </select>
-                <select name="day"style="color:#000000"class="pp">
-                  <option value="">生日を選択してください!</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                  <option value="13">13</option>
-                  <option value="14">14</option>
-                  <option value="15">15</option>
-                  <option value="16">16</option>
-                  <option value="17">17</option>
-                  <option value="18">18</option>
-                  <option value="19">19</option>
-                  <option value="20">20</option>
-                  <option value="21">21</option>
-                  <option value="22">22</option>
-                  <option value="23">23</option>
-                  <option value="24">24</option>
-                  <option value="25">25</option>
-                  <option value="26">26</option>
-                  <option value="27">27</option>
-                  <option value="28">28</option>
-                  <option value="29">29</option>
-                  <option value="30">30</option>
-                  <option value="31">31</option>
-                  </select>
-                  <br/>
-                <input type="text" name="email" placeholder="メールアドレス"/>
-                <input type="password" placeholder="パスワード(8文字以上~16文字以下)"/>
-                <input type="password" placeholder="パスワード(確認)"/>
-                <input type="submit" value="登録"/>
+									<input type="text" placeholder="性" max="10" class="name_1">
+									<input type="text" placeholder="名" max="10" class="name_2">
+							
+							<select name="gender" style="color:#666666;text-align: center;">
+								  <option value="">-- 性別 --</option>
+								  <option value="男性">男性</option>
+								  <option value="女性">女性</option>
+								</select>
+								<input type="text" class="ll" placeholder="西暦"/>
+							
+								<select name="month" style="color:#666666;text-align: center;" class="name_1">
+								  <option value="">-- 月 --</option>
+								  <option value="1">1</option>
+								  <option value="2">2</option>
+								  <option value="3">3</option>
+								  <option value="4">4</option>
+								  <option value="5">5</option>
+								  <option value="6">6</option>
+								  <option value="7">7</option>
+								  <option value="8">8</option>
+								  <option value="9">9</option>
+								  <option value="10">10</option>
+								  <option value="11">11</option>
+								  <option value="12">12</option>
+								</select>
+								<select name="day" style="color:#666666;text-align: center;" class="name_2">
+								  <option value="">-- 日 --</option>
+								  <option value="1">1</option>
+								  <option value="2">2</option>
+								  <option value="3">3</option>
+								  <option value="4">4</option>
+								  <option value="5">5</option>
+								  <option value="6">6</option>
+								  <option value="7">7</option>
+								  <option value="8">8</option>
+								  <option value="9">9</option>
+								  <option value="10">10</option>
+								  <option value="11">11</option>
+								  <option value="12">12</option>
+								  <option value="13">13</option>
+								  <option value="14">14</option>
+								  <option value="15">15</option>
+								  <option value="16">16</option>
+								  <option value="17">17</option>
+								  <option value="18">18</option>
+								  <option value="19">19</option>
+								  <option value="20">20</option>
+								  <option value="21">21</option>
+								  <option value="22">22</option>
+								  <option value="23">23</option>
+								  <option value="24">24</option>
+								  <option value="25">25</option>
+								  <option value="26">26</option>
+								  <option value="27">27</option>
+								  <option value="28">28</option>
+								  <option value="29">29</option>
+								  <option value="30">30</option>
+								  <option value="31">31</option>
+								  </select>
+								<input type="text" name="email" placeholder="メールアドレス"/>
+								<input type="password" placeholder="パスワード(8文字以上~16文字以下)"/>
+								<input type="password" placeholder="パスワード(確認)"/>
+								<input type="submit" value="登録"/>
 						</div>
 						
 					
